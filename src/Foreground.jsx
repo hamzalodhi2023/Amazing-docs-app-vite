92c6bb54d6e658b26c82841c2d8df4768ec3e88e
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-03-15 10:58:33 +0500

    interface almost completed

--- src/Foreground.jsx
+++ src/Foreground.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Cards from "./components/Cards";
 
 function Foreground() {
+  // const data = [
+  //   desc,fileSize,closeOrDownload,tagDetails
+  // ];
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing amet consectetur adipisicing.",
+      fileSize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "Hello My name is Hamza Khan Lodhi",
+      fileSize: ".5mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Hello", tagColor: "green" },
+    },
+  ];
   return (
-    <div className="fixed z-[3] w-full h-full ">
-      <Cards />
+    <div className="fixed z-[3] w-full h-full flex items-center justify-center">
+      {data.map((item, index) => (
+        <Cards data={item} />
+      ))}
     </div>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { FaRegFileAlt } from "react-icons/fa";
+import { FiDownload } from "react-icons/fi";
 
-function Cards() {
-  return <div className="w-60 h-72 bg-zinc-900 rounded-[50px]"></div>;
+function Cards({ data }) {
+  return (
+    <div className="m-10 relative w-60 h-72 bg-zinc-900 rounded-[45px] text-white  px-5 py-10 overflow-hidden">
+      <FaRegFileAlt />
+      <p className="text-sm mt-5 font-semibold leading-tight">{data.desc}</p>
+      <div className="footer absolute bottom-0 w-full   left-0">
+        <div className="flex items-center justify-between mb-3 py-3 px-8">
+          <h5>{data.fileSize}</h5>
+          <span className="flex rounded-full items-center justify-center bg-zinc-600 w-7 h-7">
+            <FiDownload color="#fff" />
+          </span>
+        </div>
+        <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
+          <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Cards;
